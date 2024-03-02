7360811702f69bfa732e7331af1c8ac82ae97f1e
pawannnkr <pawannnkr@gmail.com>
2024-03-03 01:34:53 +0530

    User Signup and signin

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path, re_path
 from . import views
@@ -6,5 +5,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     re_path('login', views.login, name='login'),
     re_path('signup', views.signup, name='signup'),
-    re_path('test_token', views.test_token, name='test_token')
+    re_path('test_token', views.test_token, name='test_token'),
+    re_path(r'^delete_user/(?P<id>\d+)/$', views.delete_user, name='delete_user/<int:id>'),
 ]
